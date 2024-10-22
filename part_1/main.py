4241157f6b2798f02e545ab093e88e0ea2a7f68a
user name <user email>
2024-10-22 20:07:28 +0200

    collections

---
+++ part_1/main.py
@@ -0,0 +1,97 @@
+class Person:
+    def __init__(self, name:str, age:int, is_active:bool, is_admin:bool):
+        self.name = name
+        self.age = age
+        self._is_active = is_active
+        self.__is_admin = is_admin
+    
+    def greating(self):
+        return f"Hi {self.name}"
+    
+    def is_active(self):
+        return self._is_active
+    
+    def set_active(self, active:bool):
+        self._is_active = active
+    def get_is_admin(self):
+        return self.__is_admin
+
+    def set_is_admin(self, is_admin: bool):
+        # Тут можна додати будь-яку логіку перевірки або обробки
+        self.__is_admin = is_admin
+
+
+p = Person("Boris", 34, True, False)
+print(p.get_is_admin())
+p.set_is_admin(True)
+print(p.get_is_admin())
+
+
+class Animal:
+    def __init__(self, nickname: str, age: int):
+        self.nickname = nickname
+        self.age = age
+
+    def make_sound(self):
+        pass
+
+class Cat(Animal):
+    def make_sound(self) -> str:
+        return "Meow"
+
+class Dog(Animal):
+
+    def __init__(self, nickname: str, age: int, breed: str):
+        super().__init__(nickname, age)  # Викликаємо конструктор базового класу
+        self.breed = breed  # Додаємо нову властивість
+
+    def make_sound(self) -> str:
+        return "Woof"
+
+    def chase_tail(self) -> str:
+        return f"{self.nickname} is chasing its tail!"
+
+class Cow(Animal):
+    def make_sound(self):
+        return "Moo"
+
+my_cat = Cat("Simon", 4)
+my_cow = Cow("Bessie", 3)
+
+print(my_cat.make_sound())  # Виведе "Meow"
+print(my_cow.make_sound())  # Виведе "Moo"
+
+my_dog = Dog("Rex", 5, "Golden Retriever")
+print(my_dog.make_sound())  # Виведе "Woof"
+print(my_dog.chase_tail())  # Виведе "Rex is chasing its tail!"
+
+
+from typing import Protocol
+
+class Speaker(Protocol):
+    def speak(self) -> str:
+        pass
+
+class Dog:
+    def speak(self) -> str:
+        return "Woof"
+
+class Cat:
+    def speak(self) -> str:
+        return "Meow"
+
+class Robot:
+    def speak(self) -> str:
+        return "Beep boop"
+
+def make_it_speak(speaker: Speaker) -> None:
+    print(speaker.speak())
+
+dog = Dog()
+cat = Cat()
+robot = Robot()
+
+make_it_speak(dog)  # Виведе: Woof
+make_it_speak(cat)  # Виведе: Meow
+make_it_speak(robot)  # Виведе: Beep boop
+
